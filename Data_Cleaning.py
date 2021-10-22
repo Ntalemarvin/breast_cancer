b68c2991a6cfe9558181f73148a1f091bb14bba6
Ntalemarvin <makvelimarvin@gmail.com>
2021-10-22 20:37:41 +0300

    added csv file

---
+++ Data_Cleaning.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 20:18:59 2021
+
+@author: marvinfrost
+"""
+
+
+import pandas as pd
+
+df = pd.read_csv('Breast_cancer_data.csv')
